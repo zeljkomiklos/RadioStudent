acf80f17893fb69aa1427acee0e6315ce36059d3
Igor Tavcar <igor.tavcar@gmail.com>
2013-12-17 15:14:37 +0100

    Initial Commit

---
+++ RadioStudent/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RadioStudent
+//
+//  Created by tigor on 17. 12. 13.
+//  Copyright (c) 2013 Zavod Radio Študent Ljubljana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RadioStudent/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RadioStudent
+//
+//  Created by tigor on 17. 12. 13.
+//  Copyright (c) 2013 Zavod Radio Študent Ljubljana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
